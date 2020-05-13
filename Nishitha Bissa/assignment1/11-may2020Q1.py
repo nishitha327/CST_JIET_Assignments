07b33e712ec6f7e01fe8b545233734970f5cc511
nishitha327 <65129589+nishitha327@users.noreply.github.com>
2020-05-13 14:36:58 +0530

    assignment1

---
+++ Nishitha Bissa/assignment1/11-may2020Q1.py
@@ -0,0 +1,13 @@
+ar= list(input('enter the array:').split())
+n=len(ar)
+def single_entry(ar,n):
+
+    r=int(ar[0])
+    
+    for i in range(1,n):
+             r=r^int(ar[i])
+    return r
+
+r=single_entry(ar,n)
+print(r)
+        
